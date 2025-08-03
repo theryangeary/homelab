69a12f32e3b2a8976557ec742ff20f6bbadd820a
Ryan Geary <rtgnj42@gmail.com>
2025-08-03 00:00:00 +0000

    don't autocomplete to empty

--- gl/rust/src/database/mod.rs
+++ gl/rust/src/database/mod.rs
@@ -140,7 +140,7 @@ impl Database {
 
     pub async fn get_suggestions(&self, query: &str) -> Result<Vec<String>> {
         let suggestions = sqlx::query_scalar::<_, String>(
-            "SELECT DISTINCT description FROM grocery_list_entries WHERE description LIKE ? ORDER BY description LIMIT 10"
+            "SELECT DISTINCT description FROM grocery_list_entries WHERE description LIKE ? AND description != '' ORDER BY description LIMIT 10"
         )
         .bind(format!("{}%", query))
         .fetch_all(&self.pool)
