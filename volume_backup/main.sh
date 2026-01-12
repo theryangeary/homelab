1790cfb3f863fa447c756ab76d8362dbb0d7140d
Ryan Geary <rtgnj42@gmail.com>
2026-01-12 00:00:00 +0000

    update volume_backup

--- volume_backup/main.sh
+++ volume_backup/main.sh
@@ -12,10 +12,11 @@ fi
 
 FTP_PASSWORD="$(cat $FTP_PASSWORD_FILE)"
 
+mkdir -p /backup
 cd /backup
 BACKUP_FILE="backup.tar.gz"
 
-tar czvf $backup_file /data
+tar czvf $BACKUP_FILE /data
 
 ftp -n $FTP_HOST 21 <<END_SCRIPT
 user ${FTP_USERNAME} ${FTP_PASSWORD}
